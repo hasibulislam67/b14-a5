beec9933c13c83155c9f2a71c209f5fb79c021e0
hasibulislam67 <hasibulislam6288@gmail.com>
2026-09-13 07:35:34 -0700

    type and tech sec

--- src/component/technologies/TechnologyCard.tsx
+++ src/component/technologies/TechnologyCard.tsx
@@ -1,1 +1,42 @@
+    import { use, type Dispatch, type SetStateAction } from "react"
+import type { devStackType } from "../../type/type"
+
+import TechnologyCard from "./TechnologyCard";
+import SelectedCard from "./SelectedCard";
+
+export interface TechnologyProps {
+    promiseData: Promise<devStackType[]>,
+    selectedTech: devStackType[],
+    setSelectedTech:Dispatch<SetStateAction<devStackType[]>>
+}
+
+export default function Technologies({ promiseData,selectedTech,setSelectedTech }: TechnologyProps) {
+    const technology = use(promiseData)
     
+
+
+    return (
+        <>
+            <section className="container mx-auto pb-20 ">
+                <h2 className="text-center md:text-start text-[24px] md:text-[36px] font-extrabold font-inter">Explore the <span className="bg-linear-to-r from-[#EC4899] via-[#BC52C7] to-[#8B5CF6] bg-clip-text text-transparent">Technologies</span></h2>
+                <p className="text-center md:text-start text-[#64748B] text-[12px] md:text-[16px]  pb-8 font-jakarta">Pick one technology per category to build your ideal stack.</p>
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-1">
+                    {/* Div of 75% */}
+                    <div className="grid md:grid-cols-3 md:col-span-3 gap-4 mx-4">
+                        {technology.map((tech) => (
+                            <TechnologyCard key={tech.id} tech={tech} selectedTech={selectedTech} setSelectedTech={setSelectedTech}></TechnologyCard>
+                        ))}
+                    </div>
+
+                    {/* Div of 25% */}
+                    <div className="flex justify-center md:justify-center  items-center md:items-start ">
+                        <div className="flex text-center md:text-start justify-center md:col-span-1 ">
+                        <SelectedCard  selectedTech={selectedTech} setSelectedTech={setSelectedTech}></SelectedCard>
+                    </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
--- src/type/type.tsx
+++ src/type/type.tsx
@@ -1,1 +1,11 @@
- 
+ export interface devStackType {
+    id: string,
+    name: string,
+    category: string,
+    description: string,
+    icon: string,
+    rating: number,
+    difficulty: string,
+    badge: string
+    badgeColor:string
+}
